f6614ac9093c39547f2acdc28fbcbb59e04fc41e
ashraful2871 <ashrafulislam2758@gmail.com>
2024-10-06 07:57:41 +0600

    show button by categories

--- script/script.js
+++ script/script.js
@@ -6,17 +6,31 @@ const loadAllPets = async () => {
 };
 loadAllPets();
 
+// load categories pets
+const loadCategories = async () => {
+  const url = "https://openapi.programming-hero.com/api/peddy/categories";
+  //   const url = "https://openapi.programming-hero.com/api/peddy/pets";
+  const res = await fetch(url);
+  const data = await res.json();
+  displayCategories(data.categories);
+};
+loadCategories();
+
+// display all pets
 const displayPets = (pets) => {
   const petsContainerEl = document.getElementById("pets-container");
   pets.forEach((pet) => {
+    // console.log(pet);
     const div = document.createElement("div");
 
     div.innerHTML = `
         <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
                         
                         <div class="card bg-base-100 p-5 w-96 border">
-                            <figure>
-                                <img src=${pet.image}
+                            <figure class="h-52">
+                                <img class="h-full w-full object-cover" src=${
+                                  pet.image
+                                }
                                     alt="pets" />
                             </figure>
                             <div class="card-body mb-2">
@@ -90,3 +104,31 @@ const displayPets = (pets) => {
     petsContainerEl.appendChild(div);
   });
 };
+
+// display pets by categories
+const displayCategories = (categories) => {
+  const btnCategoriesContainer = document.getElementById("btn-container");
+
+  categories.forEach((items) => {
+    console.log(items);
+    const btnContainer = document.createElement("div");
+    btnContainer.innerHTML = `
+        <button  onClick="loadCategoriesPets(${items.category})" class="btn font-bold text-xl px-14 rounded-full bg-[#0E7A81] text-white
+                                    lg:hover:bg-[#0E7A81] hover:text-white">${items.category}
+                    </button>
+
+    `;
+    btnCategoriesContainer.append(btnContainer);
+  });
+};
+
+// // load category pets
+// const loadCategoriesPets = async (id) => {
+//   const url = `https://openapi.programming-hero.com/api/peddy/category/${id}`;
+//   const res = await fetch(url);
+//   const data = await res.json();
+
+//   //   displayPets(data.petData.category);
+//   console.log(data);
+// };
+// loadCategoriesPets();
